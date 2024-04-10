bec5e11d2b45f4043e8bba0988626ad96b6936b9
Ibrahim <135015049+SIbrahimKashif@users.noreply.github.com>
2024-04-10 20:05:02 +0500

    Organizer.py

--- Organizer.py
+++ Organizer.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from shutil import move
 
 if __name__ == "__main__":
-    os.chdir(r"C:\Users\hp\Downloads")
+    # Path to folder
+    os.chdir(r"")
 
     # Create folders
     folders = [
@@ -23,13 +24,14 @@ if __name__ == "__main__":
 
     for file in os.listdir():
         try:
+            # Move files to respective folders based on extension
             if file.endswith((".jpg", ".png", ".jpeg")):
                 shutil.move(file, "Pictures")
             elif file.endswith((".mp3", ".wav")):
                 shutil.move(file, "Audio")
             elif file.endswith((".mp4", ".wmv", ".mkv")):
                 shutil.move(file, "Videos")
-            elif file.endswith((".ttf", ".TTF")):
+            elif file.endswith((".ttf", ".TTF", ".otf")):
                 shutil.move(file, "Fonts")
             elif file.endswith((".exe", ".rar", "msi")):
                 shutil.move(file, "Installer")
@@ -49,5 +51,6 @@ if __name__ == "__main__":
                 # Handling files with other extensions
                 if os.path.isfile(file):
                     shutil.move(file, "Others")
+        # Error handling
         except PermissionError as e:
             print(f"PermissionError: {e}")
